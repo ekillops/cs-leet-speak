e7bb4996d34491504f638ee04cd040cde803ff2f
Erik Killops and Steve Burton <erik+steve@>
2016-11-28 13:32:17 -0800

    add ability to handle the letter s at the start of a word, link bootstrap and add container

--- Objects/LeetSpeak.cs
+++ Objects/LeetSpeak.cs
@@ -6,45 +6,57 @@ namespace LeetSpeak.Objects
   {
     public string Translate(string inputString)
     {
-      char[] inputArray = inputString.ToCharArray();
+      string[] wordArray = inputString.Split(' ');
 
-      for(int i = 0; i<inputArray.Length; i++)
+      for(int j = 0; j<wordArray.Length; j++)
       {
+        char[] inputArray = wordArray[j].ToCharArray();
 
-        switch (inputArray[i])
-        {
-          case 'e':
-          case 'E':
+          for(int i = 0; i<inputArray.Length; i++)
+          {
+
+            switch (inputArray[i])
+            {
+              case 'e':
+              case 'E':
               inputArray[i] = '3';
               break;
-          case 'o':
-          case 'O':
+              case 'o':
+              case 'O':
               inputArray[i] = '0';
               break;
-          case 'a':
-          case 'A':
+              case 'a':
+              case 'A':
               inputArray[i] = '4';
               break;
-          case 'i':
-          case 'I':
+              case 'i':
+              case 'I':
               inputArray[i] = '1';
               break;
-          case 't':
-          case 'T':
+              case 't':
+              case 'T':
               inputArray[i] = '7';
               break;
-          case 's':
-              inputArray[i] = 'z';
+              case 's':
+              if (i == 0)
+              {
+                inputArray[i] = '5';
+              }
+              else
+              {
+                inputArray[i] = 'z';
+              }
               break;
-          case 'S':
+              case 'S':
               inputArray[i] = '5';
               break;
-          default:
+              default:
               break;
+            }
+          }
+          wordArray[j] = string.Join("", inputArray);
         }
-      }
-
-      string outputString = string.Join("", inputArray);
+      string outputString = string.Join(" ", wordArray);
       return outputString;
     }
   }
